e3814d4474212af5bf62c71cb54db701f58f4d55
SY <Student@Student-PC>
2024-11-25 14:48:10 +0900

    TEST

--- myapp/src/main/java/com/exam/myapp/comm/LoginInterceptor.java
+++ myapp/src/main/java/com/exam/myapp/comm/LoginInterceptor.java
@@ -18,7 +18,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 		//인자로 요청객체,응답객체,이후실행될핸들러(컨트롤러,인터셉터) 전달
 		// 핸들러(컨트롤러) 실행 전에 실행되는 메서드
 		// 반환값을 통해서, 이후 실행될 핸들러(컨트롤러,인터셉터)의 실행 여부를 결정 가능
-//		System.out.println("LoginInterceptor:preHandle...");
+		//	System.out.println("LoginInterceptor:preHandle...");
 		
 		HttpSession session = request.getSession();
 		MemberVo mvo = (MemberVo) session.getAttribute("loginUser");
